ec02e98c5a0451a80a98b4e3a955bb1fe4259d17
FranGuam <frankguansy@icloud.com>
2024-08-22 15:47:23 +0800

    fix(05): use `HashRouter` to resolve paths

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import axios from "axios";
 import { ApolloProvider } from "@apollo/client";
 import { message } from "antd";
@@ -156,7 +156,7 @@ const App = () => {
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode!);
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/login", element: <LoginPage /> },
   { path: "*", element: <App /> },
 ]);
